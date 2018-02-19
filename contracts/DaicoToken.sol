35f4e6d92fc351f61ea02298d0f8798b5c0e30ed
gleb-urvanov <gleb.urvanov@hotovo.org>
2018-02-19 15:52:16 +0100

    daico vault and daico token implemented

---
+++ contracts/DaicoToken.sol
@@ -0,0 +1,147 @@
+/**
+ * This smart contract code is Copyright 2017 TokenMarket Ltd. For more information see https://tokenmarket.net
+ *
+ * Licensed under the Apache License, version 2.0: https://github.com/TokenMarketNet/ico/blob/master/LICENSE.txt
+ */
+
+pragma solidity ^0.4.8;
+
+import "./UpgradeableToken.sol";
+import "./ReleasableToken.sol";
+import "./MintableToken.sol";
+
+
+/**
+ * A DAICO alternative to crowdsale token.
+ *
+ * An ERC-20 token designed specifically for crowdsales with investor protection and further development path.
+ *
+ * - The token transfer() is disabled until the crowdsale is over
+ * - The token contract gives an opt-in upgrade path to a new contract
+ * - The same token can be part of several crowdsales through approve() mechanism
+ * - The token can be capped (supply set in the constructor) or uncapped (crowdsale contract can mint new tokens)
+ *
+ */
+contract CrowdsaleToken is ReleasableToken, MintableToken, UpgradeableToken {
+
+  // Event listener user to notify the DAICO about token transfer.
+  ITokenEventListener public eventListener;
+
+  /** Name and symbol were updated. */
+  event UpdatedTokenInformation(string newName, string newSymbol);
+
+  string public name;
+
+  string public symbol;
+
+  uint public decimals;
+
+  /**
+   * Construct the token.
+   *
+   * This token must be created through a team multisig wallet, so that it is owned by that wallet.
+   *
+   * @param _name Token name
+   * @param _symbol Token symbol - should be all caps
+   * @param _initialSupply How many tokens we start with
+   * @param _decimals Number of decimal places
+   * @param _mintable Are new tokens created over the crowdsale or do we distribute only the initial supply? Note that when the token becomes transferable the minting always ends.
+   */
+  function CrowdsaleToken(string _name, string _symbol, uint _initialSupply, uint _decimals, bool _mintable)
+  UpgradeableToken(msg.sender) {
+
+    // Create any address, can be transferred
+    // to team multisig via changeOwner(),
+    // also remember to call setUpgradeMaster()
+    owner = msg.sender;
+
+    name = _name;
+    symbol = _symbol;
+
+    totalSupply = _initialSupply;
+
+    decimals = _decimals;
+
+    // Create initially all balance on the team multisig
+    balances[owner] = totalSupply;
+
+    if(totalSupply > 0) {
+      Minted(owner, totalSupply);
+    }
+
+    // No more new supply allowed after the token creation
+    if(!_mintable) {
+      mintingFinished = true;
+      if(totalSupply == 0) {
+        throw; // Cannot create a token without supply and no minting
+      }
+    }
+  }
+
+  /**
+   * When token is released to be transferable, enforce no new tokens can be created.
+   */
+  function releaseTokenTransfer() public onlyReleaseAgent {
+    mintingFinished = true;
+    super.releaseTokenTransfer();
+  }
+
+  /**
+   * Allow upgrade agent functionality kick in only if the crowdsale was success.
+   */
+  function canUpgrade() public constant returns(bool) {
+    return released && super.canUpgrade();
+  }
+
+  /**
+   * Owner can update token information here.
+   *
+   * It is often useful to conceal the actual token association, until
+   * the token operations, like central issuance or reissuance have been completed.
+   *
+   * This function allows the token owner to rename the token after the operations
+   * have been completed and then point the audience to use the token contract.
+   */
+  function setTokenInformation(string _name, string _symbol) onlyOwner {
+    name = _name;
+    symbol = _symbol;
+
+    UpdatedTokenInformation(name, symbol);
+  }
+
+  /**
+* @dev Set/remove token event listener
+* @param _listener Listener address (Contract must implement ITokenEventListener interface)
+*/
+  function setListener(address _listener) public onlyOwner {
+    if(_listener != address(0)) {
+      eventListener = ITokenEventListener(_listener);
+    } else {
+      delete eventListener;
+    }
+  }
+
+  function hasListener() internal view returns(bool) {
+    if(eventListener == address(0)) {
+      return false;
+    }
+    return true;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    bool isSuccessful = super.transfer(_to, _value);
+    if (hasListener() && isSuccessful) {
+      eventListener.onTokenTransfer(msg.sender, _to, _value);
+    }
+    return isSuccessful;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    bool isSuccessful = super.transferFrom(_from, _to, _value);
+    if (hasListener() && isSuccessful) {
+      eventListener.onTokenTransfer(_from, _to, _value);
+    }
+    return isSuccessfull;
+  }
+
+}
---
+++ contracts/DaicoVault.sol
@@ -0,0 +1,164 @@
+
+pragma solidity ^0.4.18;
+
+import "./SafeMathLib.sol";
+import "./FractionalERC20.sol";
+
+contract DaicoVault is ITokenEventListener {
+  using SafeMathLib for uint;
+
+  // The token investors are holding
+  FractionalERC20 public token;
+
+  uint public debatingPeriodInMinutes;
+  uint public majorityMargin;
+  Proposal[] public proposals;
+  uint public numProposals;
+
+  event ProposalAdded(uint proposalID, address recipient, uint amount, string description);
+  event Voted(uint proposalID, bool position, address voter, string justification);
+  event ProposalTallied(uint proposalID, uint positiveResult, uint negativeResult, bool active);
+
+  struct Vote {
+    bool inSupport;
+    address voter;
+    string justification;
+  }
+
+  struct Proposal {
+    address recipient;
+    uint amount;
+    string description;
+    uint votingDeadline;
+    bool executed;
+    bool proposalPassed;
+    uint numberOfVotes;
+    uint currentResult;
+    uint positiveResult;
+    uint negativeResult;
+    mapping (address => bool) supported;
+    mapping (address => uint) voted;
+  }
+
+  // Modifier that allows only shareholders to vote and create new proposals
+  modifier onlyMembers {
+    require(token.balanceOf(msg.sender) != 0);
+    _;
+  }
+
+  modifier noOngoingProposals {
+    require ((numProposals == 0) || (proposals[numProposals - 1].executed));
+    _;
+  }
+
+  function DaicoVault(uint minutesForDebate, uint marginOfVotesForMajority,
+    FractionalERC20 daicoToken) public {
+    debatingPeriodInMinutes = minutesForDebate;
+    majorityMargin = marginOfVotesForMajority;
+    token = daicoToken;
+  }
+
+  function () public payable {
+  }
+
+  /**
+    * Add Proposal
+    *
+    * Propose to send `weiAmount / 1e18` ether to `beneficiary` for `jobDescription`. `transactionBytecode ? Contains : Does not contain` code.
+    *
+    * @param beneficiary who to send the ether to
+    * @param weiAmount amount of ether to send, in wei
+    * @param jobDescription Description of job
+    */
+  function newProposal(address beneficiary, uint weiAmount, string jobDescription)
+  onlyMembers noOngoingProposals public returns (uint proposalID) {
+
+    proposalID = proposals.length++;
+    Proposal storage p = proposals[proposalID];
+    p.recipient = beneficiary;
+    p.amount = weiAmount;
+    p.description = jobDescription;
+    p.votingDeadline = now + debatingPeriodInMinutes * 1 minutes;
+    p.executed = false;
+    p.proposalPassed = false;
+    ProposalAdded(proposalID, beneficiary, weiAmount, jobDescription);
+    numProposals = proposalID+1;
+
+    return proposalID;
+  }
+
+  /**
+    * Log a vote for a proposal
+    *
+    * Vote `supportsProposal? in support of : against` proposal #`proposalNumber`
+    *
+    * @param proposalNumber number of proposal
+    * @param supportsProposal either in favor or against it
+    * @param justificationText optional justification text
+    */
+  function vote(uint proposalNumber, bool supportsProposal, string justificationText)
+  onlyMembers public {
+    require (p.voted[msg.sender] == 0);
+    Proposal storage p = proposals[proposalNumber];         // Get the proposal
+    p.voted[msg.sender] = token.balanceOf(msg.sender);      // Set this voter as having voted
+    if (supportsProposal) {                         // If they support the proposal
+      p.positiveResult = plus(p.positiveResult, p.voted[msg.sender]);  // Increase score
+      p.supported[msg.sender] = true;
+    } else {                                        // If they don't
+      p.negativeResult = minus(p.negativeResult, p.voted[msg.sender]); // Decrease the score
+      p.supported[msg.sender] = false;
+    }
+
+    // Create a log of this event
+    Voted(proposalNumber, supportsProposal, msg.sender, justificationText);
+  }
+
+  /**
+     * Finish vote
+     *
+     * Count the votes proposal #`proposalNumber` and execute it if approved
+     *
+     * @param proposalNumber proposal number
+     */
+  function approveFunding(uint proposalNumber) public {
+    Proposal storage p = proposals[proposalNumber];
+
+    require(now > p.votingDeadline                                            // If it is past the voting deadline
+    && !p.executed);                                  // and a minimum quorum has been reached...
+
+    // ...then execute result
+
+    uint result = minus(p.positiveResult, p.negativeResult);
+    if (result > majorityMargin) {
+      // Proposal passed; execute the transaction
+
+      p.executed = true; // Avoid recursive calling
+      require(p.recipient.call.value(p.amount)());
+
+      p.proposalPassed = true;
+    } else {
+      // Proposal failed
+      p.proposalPassed = false;
+    }
+
+    // Fire Events
+    ProposalTallied(proposalNumber, p.positiveResult, p.negativeResult, p.proposalPassed);
+  }
+
+  function onTokenTransfer(address _from, address _to, uint256 _value) public {
+    require (msg.sender() == token);
+    if (numProposals == 0) {
+      return;
+    }
+    Proposal currentProposal = proposals[numProposals - 1];
+    if ((currentProposal.executed) || (token.balanceOf(_from) > p.voted[_from])) {
+        return;
+    }
+    currentProposal.voted[_from] = token.balanceOf(_from);
+    if (currentProposal.voted[_to] != 0) {
+      currentProposal.voted[_to] = token.balanceOf(_to);
+    }
+
+  }
+
+}
